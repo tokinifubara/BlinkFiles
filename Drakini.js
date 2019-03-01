77c20b680d4c238489f27509cbd1816d2f012ec8
tokinifubara <teekaymtok@gmail.com>
2019-03-01 08:45:26 +0000

    Changed opacity of cursor

--- Drakini.js
+++ Drakini.js
@@ -1,5 +1,5 @@
 t.prefs_.set('color-palette-overrides',["#000000", "#ff5555", "#50fa7b", "#f1fa8c", "#bd93f9", "#ff79c6", "#8be9fd", "#bbbbbb", "#555555", "#ff5555", "#50fa7b", "#f1fa8c", "#bd93f9", "#ff79c6", "#8be9fd", "#ffffff"]);
 t.prefs_.set('foreground-color', "#f8f8f2");
 t.prefs_.set('background-color', "#1e1f29");
-t.prefs_.set('cursor-color', 'rgba(46, 182, 255, 0.5)');
+t.prefs_.set('cursor-color', 'rgba(46, 182, 255, 1)');
 t.prefs_.set('cursor-blink', true);
