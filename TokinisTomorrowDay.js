b5d00ddac7687424e05558ebe7652d860870f626
tokinifubara <teekaymtok@gmail.com>
2019-03-07 09:58:43 +0000

    Tomorrow Day with blue cursor

---
+++ TokinisTomorrowDay.js
@@ -0,0 +1,5 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#c82829", "#718c00", "#eab700", "#4271ae", "#8959a8", "#3e999f", "#ffffff", "#000000", "#c82829", "#718c00", "#eab700", "#4271ae", "#8959a8", "#3e999f", "#ffffff"]);
+t.prefs_.set('foreground-color', "#4d4d4c");
+t.prefs_.set('background-color', "#ffffff");
+t.prefs_.set('cursor-color', 'rgba(46, 182, 255, 1)');
+t.prefs_.set('cursor-blink', true);
